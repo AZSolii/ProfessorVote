e4c88d35a96532434c3ffaa6fa88880db0a9eccf
Andres Castro <acastr7@gmail.com>
2012-02-20 18:42:02 -0500

    Basic file structure for the website.

---
+++ app/views/home_page.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Welcome Professor Vote!</title>
+<link href='http://fonts.googleapis.com/css?family=Quantico' rel='stylesheet' type='text/css'>
+<style type="text/css">
+@font-face {
+    font-family: 'AirstripFourRegular';
+    src: url('/ci/fonts/airstrip-webfont.ttf');
+   /* src: url('/public_html/system/fonts/airstrip-webfont.eot?#iefix') format('embedded-opentype'),
+         url('/public_html/system/fonts/airstrip-webfont.woff') format('woff'),
+         url('/public_html/system/fonts/airstrip-webfont.ttf') format('truetype'),
+         url('/public_html/system/fonts/airstrip-webfont.svg#AirstripFourRegular') format('svg'); 
+    font-weight: normal;
+    font-style: normal; */
+
+}
+
+::selection {
+	background-color: #E13300;
+	color: white;
+}
+
+::moz-selection {
+	background-color: #E13300;
+	color: white;
+}
+
+::webkit-selection {
+	background-color: #E13300;
+	color: white;
+}
+
+body {
+	background-color: #fff;
+	margin: 100px;
+	font: 13px/20px normal Helvetica, Arial, sans-serif;
+	color: #4F5155;
+}
+
+a {
+	color: #003399;
+	background-color: transparent;
+	font-weight: normal;
+}
+
+h1 {
+	font-family: 'Quantico', cursive;
+	font: AirstripFourRegular
+	color: #444;
+	background-color: transparent;
+	border-bottom: 1px solid #D0D0D0;
+	font-size: 50px;
+	font-weight: normal;
+	margin: 0 0 14px 0;
+	padding: 14px 15px 10px 15px;
+	text-align:center
+}
+
+
+#body {
+	margin: 0 15px 0 15px;
+	text-align:center
+}
+
+p.footer {
+	text-align: right;
+	font-size: 11px;
+	border-top: 1px solid #D0D0D0;
+	line-height: 32px;
+	padding: 0 10px 0 10px;
+	margin: 20px 0 0 0;
+}
+
+#container {
+	margin: 10px;
+	border: 2px solid #D0D0D0;
+	-webkit-box-shadow: 0 0 8px #D0D0D0;
+}
+</style>
+</head>
+<body>
+
+	<div id="container">
+		<h1>Welcome to Professor Vote!!</h1>
+
+		<div id="body">
+			<p>
+				Site Under Development!
+			</p>
+		</div>
+
+		<p class="footer">
+			Page rendered in <strong>{elapsed_time}</strong> seconds
+		</p>
+	</div>
+
+</body>
+</html>
