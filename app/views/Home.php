56fc47a589a1518ef854bf40d15011ee41fb7621
Andres Castro <acastr7@gmail.com>
2012-02-25 15:59:08 -0500

    New Home Page Part 2!

---
+++ app/views/Home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>"Welcome To Professor Vote"</title>
+		<!-- Le styles -->
+		<link href="../css/bootstrap.css" rel="stylesheet">
+		<style>
+			body {
+				padding-top: 60px;
+				/* 60px to make the container go all the way to the bottom of the topbar */
+			}
+			.topbar .btn {
+				border: 0;
+			}
+		</style>
+		<link href="../css//bootstrap-responsive.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
+					<a class="brand" href="#">ProfessorVote.com</a>
+					<div class="nav-collapse">
+						<form class="form-inline pull-right" style="display: inline; margin-bottom:
+						0; margin-left: 15px">
+							<ul class="nav">
+								<li>
+									<a href="#register">Register</a>
+								</li>
+							</ul>
+							<input type="text" placeholder="Email" class="input-small">
+							<input type="password" placeholder="Password" class="input-small">
+							<button class="btn" type="submit" style="margin: 5px">
+								Go
+							</button>
+						</form>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<!-- Main hero unit for a primary marketing message or call to action -->
+			<div class="hero-unit">
+				<h1>Welcome!</h1>
+				<p>
+					ProfessorVote.com is a new and unique way of rating your College professors and getting a quick and easy view of the best professors at your school.
+				</p>
+				<p>
+					<a class="btn btn-primary btn-large">Learn more &raquo;</a>
+				</p>
+			</div>
+			<hr>
+			<footer>
+				<p>
+					&copy; ProfessorVote.com 2012 --  
+					Page rendered in <strong>{elapsed_time}</strong> seconds
+				</p>
+			</footer>
+		</div>
+		<!-- /container -->
+		<!-- Le javascript
+		=============================s===================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="../scripts/jquery-1.7.1.min.js"></script>
+		<script src="../scripts/bootstrap.min.js"></script>
+	</body>
+</html>
