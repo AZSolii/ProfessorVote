038895c14bc95933117b88b5a0efceba493c09d5
John Ulman <johnulman@gmail.com>
2012-02-28 19:17:01 -0500

    another !

--- app/views/HomePage.php
+++ app/views/HomePage.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<!-- Main hero unit for a primary marketing message or call to action -->
 	<div class="hero-unit">
-		<h1>Welcome!!</h1>
+		<h1>Welcome!!!</h1>
 		<p>
 			ProfessorVote.com is a new and unique way of rating your College professors and getting a quick and easy view of the best professors at your school. We are currently under development
 			so please stand by as we will be fully operational in two months.
